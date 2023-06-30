77207a1e5b61fcf3788dcea04309d34cd614d892
mohamadhoseini6570 <mohamadhoseini6570@gmail.com>
2023-06-30 10:50:10 +0330

    after creating project and apps-020409

---
+++ cloud/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+# from django.views.generic import TemplateView
+from . import views
+# from rest_framework import routers
+# from django.conf.urls import include
+
+app_name = 'cloud'
+
+urlpatterns = [
+    # path('rest/', include(router.urls)),
+]
