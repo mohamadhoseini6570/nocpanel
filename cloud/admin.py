e24be8f41ae48e29e9b19060d99cb7d222982682
mohamadhoseini6570 <mohamadhoseini6570@gmail.com>
2023-06-30 15:56:39 +0330

    after adding cloud model-020409

--- cloud/admin.py
+++ cloud/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class CloudAdmin(admin.ModelAdmin):
+    admin.site.register(models.Cloud)
--- cloud/models.py
+++ cloud/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from contract.models import Contract
+from customer.models import Customer
+from agent.models import Agent
 
-# Create your models here.
+class Cloud(models.Model):
+    sizechoices = (
+        ('Micro', 'Micro'),
+        ('Small', 'Small'),
+        ('Medium', 'Medium'),
+        ('Large', 'Large'),
+        ('XLarge', 'XLarge'),
+        ('XXLarge', 'XXLarge'),
+    )
+    size = models.CharField(max_length=7, choices=sizechoices, default='Medium')
+    ip = models.GenericIPAddressField(verbose_name='آدرس IP')
+    notes = models.CharField(max_length=500, verbose_name='توضیحات',blank=True, null=True)
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)  
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    agents = models.ManyToManyField(Agent, blank=True)
+
+    def __str__(self):
+        return self.size
+
+    # def get_absolute_url(self):
+    #     return reverse('customerservice:cloud-detail-list', args=[str(self.id)])
+
+    class Meta:
+        ordering = ['id']
